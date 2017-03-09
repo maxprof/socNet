56a8a34722288cdd3bf4e07766d5041fb3102a0d
MaxGTa <MaxGTa>
2017-03-09 15:27:43 +0200

    remove MONGODB variable

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var env = process.env.NODE_ENV || 'dev';
-var MONGODB_URI = 'mongodb://heroku_58x9d265:5fr41lapckuscd4h5remlqeo54@ds019966.mlab.com:19966/heroku_58x9d265';
 var config = {
     production: {},
     dev: {
         port: process.env.PORT || 5000,
-        db: process.env.MONGODB_URI || MONGODB_URI || 'mongodb://localhost:27017/SocNet',
+        db: process.env.MONGODB_URI || 'mongodb://localhost:27017/SocNet',
         sessionSecret: 'superSecret',
         tokenName: 'superToken',
         files: 'public/uploads'
